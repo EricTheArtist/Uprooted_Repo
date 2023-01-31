411d486b624d5f16b1b73f61bdc319ff0832e4a7
MamelloPhambili <53046693+MamelloRaboroko@users.noreply.github.com>
2023-01-31 19:31:45 +0200

    Created NavMesh for Red Ant
    Move tire around in scene mode, then press enter in play mode to test it out

---
+++ Assets/Mamello_Scripts/RedAntNavMesh.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RedAntNavMesh : MonoBehaviour
+{
+    [SerializeField] private Transform moveTarget;
+
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //making RedAnt movetowards target
+            navMeshAgent.destination = moveTarget.position;
+        }
+       
+    }
+}
