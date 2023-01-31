85fa98beb8192e9235f772e706eb0aa829ab0563
Eric <ericgardiner10@gmail.com>
2023-01-31 14:01:48 +0200

    Completed Tile placement system and imported assets of houses

--- Assets/Eric_Assets/Scripts/BuildingSystem.cs
+++ Assets/Eric_Assets/Scripts/BuildingSystem.cs
@@ -31,6 +31,33 @@ public class BuildingSystem : MonoBehaviour
         {
             InitializeWithObject(prefab1);
         }
+
+        if (!objectToPlace)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            objectToPlace.Rotate();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (CanBePlaced(objectToPlace))
+            {
+                objectToPlace.Place();
+                Vector3Int start = gridLayout.WorldToCell(objectToPlace.GetStartPosition());
+                TakeArea(start, objectToPlace.Size);
+            }
+            else
+            {
+                Destroy(objectToPlace.gameObject);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Destroy(objectToPlace.gameObject);
+        }
     }
     #endregion
 
@@ -54,6 +81,22 @@ public class BuildingSystem : MonoBehaviour
         position = grid.GetCellCenterWorld(cellPos);
         return position;
     }
+
+    private static TileBase[] GetTilesBlock(BoundsInt area, Tilemap tilemap)
+    {
+        TileBase[] array = new TileBase[area.size.x * area.size.y * area.size.z];
+        int counter = 0;
+
+        foreach (var v in area.allPositionsWithin)
+        {
+            Vector3Int pos = new Vector3Int(v.x, v.y, 0);
+            array[counter] = tilemap.GetTile(pos);
+            counter++;
+        }
+
+        return array;
+    }
+
     #endregion
 
     #region Building Placement
@@ -66,6 +109,33 @@ public class BuildingSystem : MonoBehaviour
         objectToPlace = obj.GetComponent<PlacableObject>();
         obj.AddComponent<ObjectDrag>();
     }
+
+    private bool CanBePlaced(PlacableObject placableObject)
+    {
+        BoundsInt area = new BoundsInt();
+        area.position = gridLayout.WorldToCell(objectToPlace.GetStartPosition());
+        area.size = placableObject.Size;
+        area.size = new Vector3Int(area.size.x + 1, area.size.y + 1, area.size.z);
+
+        TileBase[] baseArray = GetTilesBlock(area, MainTilemap);
+
+        foreach (var b in baseArray)
+        {
+            if(b == whiteTile)
+            {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    public void TakeArea(Vector3Int start, Vector3Int size)
+    {
+        MainTilemap.BoxFill(start, whiteTile, start.x, start.y, start.x + size.x, start.y + size.x);
+    }
+
 
     #endregion
     // Update is called once per frame
--- Assets/Eric_Assets/Scripts/PlacableObject.cs
+++ Assets/Eric_Assets/Scripts/PlacableObject.cs
@@ -4,10 +4,66 @@ using UnityEngine;
 
 public class PlacableObject : MonoBehaviour
 {
+    public bool Placed { get; private set; }
+    public Vector3Int Size { get; private set; }
+
+    private Vector3[] Vertices;
     // Start is called before the first frame update
+
+    private void GetColliderVertexPositionsLocal()
+    {
+        BoxCollider b = gameObject.GetComponent<BoxCollider>();
+        Vertices = new Vector3[4];
+        Vertices[0] = b.center + new Vector3(-b.size.x, -b.size.z) * 0.5f;
+        Vertices[1] = b.center + new Vector3(b.size.x, -b.size.z) * 0.5f;
+        Vertices[2] = b.center + new Vector3(b.size.x, b.size.z) * 0.5f;
+        Vertices[3] = b.center + new Vector3(-b.size.x, b.size.z) * 0.5f;
+    }
+
+    private void CalculateSizeInCells()
+    {
+        Vector3Int[] vertices = new Vector3Int[Vertices.Length];
+
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 worldPos = transform.TransformPoint(Vertices[i]);
+            vertices[i] = BuildingSystem.current.gridLayout.WorldToCell(worldPos);
+        }
+
+        Size = new Vector3Int(Mathf.Abs((vertices[0] - vertices[1]).x), 
+            Mathf.Abs((vertices[0] - vertices[1]).y), 1);
+    }
+
+    public Vector3 GetStartPosition()
+    {
+        return transform.TransformPoint(Vertices[0]);
+    }
     void Start()
     {
-        
+        GetColliderVertexPositionsLocal();
+        CalculateSizeInCells();
+    }
+
+    public void Rotate()
+    {
+        transform.Rotate(new Vector3(0, 90, 0));
+        Size = new Vector3Int(Size.y, Size.x, 1);
+
+        Vector3[] vertices = new Vector3[Vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = Vertices[(i + 1) % Vertices.Length];
+        }
+        Vertices = vertices;
+    }
+
+    public virtual void Place()
+    {
+        ObjectDrag drag = gameObject.GetComponent<ObjectDrag>();
+        Destroy(drag);
+        Placed = true;
+
+        // invoke events of placement (Build timer etc);
     }
 
     // Update is called once per frame
